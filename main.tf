2418f273c0ea89252c17ecb27d3fef44b691c6e8
Shabeer1024 <shabeer.shukoor@outlook.com>
2026-05-07 13:50:55 +0530

    only have the resource group adding tags

--- main.tf
+++ main.tf
@@ -2,4 +2,5 @@ module "resource_group" {
   source              = "./modules/resourcegroup"
   resource_group_name = var.resource_group_name
   location            = var.location
+  tags = var.tags
 }
--- modules/resourcegroup/variables.tf
+++ modules/resourcegroup/variables.tf
@@ -1,27 +1,17 @@
 variable "resource_group_name" {
-  description = "Name of the resource group for the AVD lab"
-  type        = string
-  default     = "rg-avd-lab-eastus"
-
-  validation {
-    condition     = length(var.resource_group_name) > 0 && length(var.resource_group_name) <= 90
-    error_message = "Resource group name must be 1-90 characters."
-  }
+  type = string
 }
 
 variable "location" {
-  description = "Azure region where resources will be created"
-  type        = string
-  default     = "East US"
+  type = string
 }
 
 variable "tags" {
-  description = "Common tags applied to all resources in the lab"
+  description = "Common tags applied to all resources"
   type        = map(string)
   default = {
     Environment = "Lab"
     Project     = "AVD-Lab"
-    ManagedBy   = "Terraform"
     Owner       = "Shabeer"
     CostCenter  = "Learning"
   }
